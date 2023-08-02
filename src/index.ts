ab957f89449c69c6408d1ac8e3ee45efc042674d
Nick Linnell <nick@kyanmedia.com>
2023-08-02 17:16:58 +0100

    Default date to last day or previous month

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,14 @@ import { User } from './types';
 
 getUsers().then((departmentsMap: { [key: string]: User[] }) => {
   const dateArg = process.argv[2];
-  const date = dateArg ? new Date(dateArg) : new Date();
+  let date;
+  if (dateArg) {
+    date = new Date(dateArg);
+  } else {
+    date = new Date();
+    date.setMonth(date.getMonth() - 1);
+    date.setDate(new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate());
+  }
 
   getBillability(departmentsMap, date).catch((error: string) => {
     console.error(`Error: ${error}`);
